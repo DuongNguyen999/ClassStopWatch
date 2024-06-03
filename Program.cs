43d0863f43fcfc4ced30d093484a67bd58c00579
DuongNguyen999 <165481404+DuongNguyen999@users.noreply.github.com>
2024-06-03 23:24:18 +0900

    Add
    Add

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] array = new int[100000];
+        Random random = new Random();
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(1, 100000);
+        }
+
+        StopWatch stopwatch = new StopWatch();
+        stopwatch.Start();
+        SelectionSort(array);
+        stopwatch.Stop();
+        double elapsedTime = stopwatch.GetElapsedTime();
+        Console.WriteLine("Elapsed time for selection sort: " + elapsedTime + " milliseconds");
+    }
+    public static void SelectionSort(int[] array)
+    {
+        int n = array.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++)
+            {
+                if (array[j] < array[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+    }
+}
---
+++ StopWatch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class StopWatch
+{
+    private DateTime startTime;
+    private DateTime endTime;
+    public StopWatch()
+    {
+        startTime = DateTime.Now;
+    }
+    public DateTime StartTime
+    {
+        get { return startTime; }
+    }
+    public DateTime EndTime
+    {
+        get { return endTime; }
+    }
+    public void Start()
+    {
+        startTime = DateTime.Now;
+    }
+    public void Stop()
+    {
+        endTime = DateTime.Now;
+    }
+    public double GetElapsedTime()
+    {
+        return (endTime - startTime).TotalMilliseconds;
+    }
+}
